e885739ebf28b332dfcad796677ba1eb9fb2d83c
dvize <dvize404@gmail.com>
2023-10-08 22:23:03 -0700

    update fins eerie silence for 3.7

--- Fin-EerieSilence/src/mod.ts
+++ Fin-EerieSilence/src/mod.ts
@@ -1,6 +1,5 @@
-// license: NCSA 
-// copyright: Fin 
-// website: Nada	
+// license: NCSA
+// copyright: Fin
 // authors: Fin, Props (updated)
 
 import { DependencyContainer } from "tsyringe";
@@ -8,91 +7,76 @@ import { IPostDBLoadMod } from "@spt-aki/models/external/IPostDBLoadMod";
 import { ILogger } from "@spt-aki/models/spt/utils/ILogger";
 import { DatabaseServer } from "@spt-aki/servers/DatabaseServer";
 
-
 //Code
-class Mod implements IPostDBLoadMod
-{
-	private config = require("../config/config.json");
-	
-	
+class Mod implements IPostDBLoadMod {
+  private config = require("../config/config.json");
 
-	public postDBLoad(container: DependencyContainer): void 
-	{	
-		//Get DB Server after load.
-		let databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
-		const logger = container.resolve<ILogger>("WinstonLogger");
-		
-		this.quieterHeadsets(databaseServer, logger);
-		
-		
-	}
-		
-	public quieterHeadsets(databaseServer, logger): void
-	{
-		
-		const config = require("../config/config.json");
-		var database = databaseServer.getTables();	
-		
-		logger.info('EerieSilence: Searching for Headphone and Deafness Related Items');
-		
-		for (let itemID in database.templates.items)
-		{
-			let item = database.templates.items[itemID];
-			let itemname = item._name;
-			
-			//check if the parent is of type headphone
-			if (item._parent == "5645bcb74bdc2ded0b8b4578") 
-			{
-				if (config.full_Stat_Control)
-				{
-					for (let stat in config.stats){
-						item._props[stat] = config.stats[stat];
-					}	
-					//logger.info(`EerieSilence: Setting full stats for ${itemname}`);
-				}
-				else {
+  public postDBLoad(container: DependencyContainer): void {
+    //Get DB Server after load.
+    let databaseServer = container.resolve<DatabaseServer>("DatabaseServer");
+    const logger = container.resolve<ILogger>("WinstonLogger");
 
-					item._props.Distortion = item._props.Distortion * config.distortion_Multiplier;
-					item._props.AmbientVolume = item._props.AmbientVolume * config.ambient_noise_reduction_multiplier;
-					//logger.info(`EerieSilence: Setting custom Distortion and AmbientVolume for ${itemname}`);
-				}
-				
-				
-			}
-			
-			//also if they item has DeafStrength Property
-			if (item._props.DeafStrength && item._parent == "5a341c4086f77401f2541505")
-			{
-				switch (config.maximum_helmet_deafness.toLowerCase())
-				{
-					case "high":
-					{
-						item._props.DeafStrength = "High";
-						//logger.info(`EerieSilence: Setting High DeafStrength for ${itemname}`);
-						break;
-					}
-					
-					case "low": 
-					{
-							item._props.DeafStrength = "Low";
-							//logger.info(`EerieSilence: Setting Low DeafStrength for ${itemname}`);
-							break;
-					}
-					case "none": 
-					{
-							item._props.DeafStrength = "None";
-							//logger.info(`EerieSilence: Setting None DeafStrength for ${itemname}`);
-							break;
-					}
-					default:
-					{
-						break;
-					}
-				}
-			}
-		}
-	}
-	
+    this.quieterHeadsets(databaseServer, logger);
+  }
+
+  public quieterHeadsets(databaseServer, logger): void {
+    const config = require("../config/config.json");
+    var database = databaseServer.getTables();
+
+    logger.info(
+      "EerieSilence: Searching for Headphone and Deafness Related Items"
+    );
+
+    for (let itemID in database.templates.items) {
+      let item = database.templates.items[itemID];
+      let itemname = item._name;
+
+      //check if the parent is of type headphone
+      if (item._parent == "5645bcb74bdc2ded0b8b4578") {
+        if (config.full_Stat_Control) {
+          for (let stat in config.stats) {
+            item._props[stat] = config.stats[stat];
+          }
+          //logger.info(`EerieSilence: Setting full stats for ${itemname}`);
+        } else {
+          item._props.Distortion =
+            item._props.Distortion * config.distortion_Multiplier;
+          item._props.AmbientVolume =
+            item._props.AmbientVolume *
+            config.ambient_noise_reduction_multiplier;
+          //logger.info(`EerieSilence: Setting custom Distortion and AmbientVolume for ${itemname}`);
+        }
+      }
+
+      //also if they item has DeafStrength Property
+      if (
+        item._props.DeafStrength &&
+        item._parent == "5a341c4086f77401f2541505"
+      ) {
+        switch (config.maximum_helmet_deafness.toLowerCase()) {
+          case "high": {
+            item._props.DeafStrength = "High";
+            //logger.info(`EerieSilence: Setting High DeafStrength for ${itemname}`);
+            break;
+          }
+
+          case "low": {
+            item._props.DeafStrength = "Low";
+            //logger.info(`EerieSilence: Setting Low DeafStrength for ${itemname}`);
+            break;
+          }
+          case "none": {
+            item._props.DeafStrength = "None";
+            //logger.info(`EerieSilence: Setting None DeafStrength for ${itemname}`);
+            break;
+          }
+          default: {
+            break;
+          }
+        }
+      }
+    }
+  }
 }
 
-module.exports = { mod: new Mod() }
+module.exports = { mod: new Mod() };
